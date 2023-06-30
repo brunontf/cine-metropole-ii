e4f6d345de28835ec6979f97c2ca8d1ae0b166a7
brunontf <brunontf@gmail.com>
2023-06-29 22:51:25 -0300

    gostei

--- src/app/components/lista-filmes/lista-filmes.component.ts
+++ src/app/components/lista-filmes/lista-filmes.component.ts
@@ -12,10 +12,36 @@ export class ListaFilmesComponent {
   constructor(private filmeData:FilmeDataService){}
 
   listaFilmes = this.filmeData.listaFilmes;
+  // like : string = "bi bi-hand-thumbs-up";
+  // unlike : string = "bi bi-hand-thumbs-down";
+
+  liker(gostei:HTMLElement,naoGostei:HTMLElement, gost:HTMLAnchorElement){
+    if(gostei.className == "bi bi-hand-thumbs-up"){
+      gostei.className = "bi bi-hand-thumbs-up-fill";
+    } else {
+      gostei.className = "bi bi-hand-thumbs-up";
+    }
+    if(naoGostei.className == "bi bi-hand-thumbs-down-fill"){
+      naoGostei.className = "bi bi-hand-thumbs-down";
+    }
+    // console.log(gost.);
+  }
+
+  unliker(naoGostei:HTMLElement,gostei:HTMLElement){
+    if(naoGostei.className == "bi bi-hand-thumbs-down"){
+      naoGostei.className = "bi bi-hand-thumbs-down-fill";
+    } else {
+      naoGostei.className = "bi bi-hand-thumbs-down";
+    }
+    if(gostei.className == "bi bi-hand-thumbs-up-fill"){
+      gostei.className = "bi bi-hand-thumbs-up";
+    }
+  }
+
   
   removerFilme(id:string){
-    this.listaFilmes  = this.listaFilmes.filter(item => { return item.id !== id});
-    this.listaFilmes .forEach((item,i) => item.id = (i+1).toString())
+    this.listaFilmes  = this.listaFilmes.filter(item => item.id !== id);
+    this.listaFilmes.forEach((item,i) => item.id = (i+1).toString());
     console.log(this.listaFilmes);
   }
 }
